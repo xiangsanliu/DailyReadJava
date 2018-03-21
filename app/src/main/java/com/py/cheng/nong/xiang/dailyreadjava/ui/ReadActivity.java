097edb1d8e9a0ad8b4ec1df4344229235a56c528
xiang <xiangjianjian@hotmail.com>
2018-03-21 18:23:43 +0800

    几乎完成了

--- app/src/main/java/com/py/cheng/nong/xiang/dailyreadjava/ui/ReadActivity.java
+++ app/src/main/java/com/py/cheng/nong/xiang/dailyreadjava/ui/ReadActivity.java
@@ -1,9 +1,14 @@
 package com.py.cheng.nong.xiang.dailyreadjava.ui;
 
+import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.graphics.Color;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 
@@ -12,12 +17,14 @@ import com.py.cheng.nong.xiang.dailyreadjava.R;
 import com.py.cheng.nong.xiang.dailyreadjava.databinding.ActivityReadBinding;
 import com.py.cheng.nong.xiang.dailyreadjava.model.bean.Article;
 import com.py.cheng.nong.xiang.dailyreadjava.presenter.ReadPresenter;
+import com.py.cheng.nong.xiang.dailyreadjava.share.CollapsingToolbarLayoutState;
 import com.py.cheng.nong.xiang.dailyreadjava.view.ReadView;
 
 public class ReadActivity extends AppCompatActivity implements ReadView {
 
     private ActivityReadBinding binding;
     private ReadPresenter presenter;
+    private CollapsingToolbarLayoutState state = CollapsingToolbarLayoutState.EXPANDED;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,5 +72,40 @@ public class ReadActivity extends AppCompatActivity implements ReadView {
         //加载文章并，载入css
         binding.markdownView.loadMarkdown(article.getBody(), "file:///android_asset/style.css");
         Glide.with(this).load(article.getImage()).into(binding.background);
+        binding.appBar.addOnOffsetChangedListener((appBarLayout, verticalOffset) -> {
+            if (verticalOffset == 0) {
+                //展开状态栏
+                if (state != CollapsingToolbarLayoutState.EXPANDED) {
+                    state = CollapsingToolbarLayoutState.EXPANDED;
+                    binding.toolbarLayout.setTitle(" ");
+                    binding.articleTitle.setText(article.getTitle());
+                    invalidateOptionsMenu();
+                }
+            } else if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
+                //折叠状态栏
+                binding.toolbarLayout.setTitle(article.getTitle());
+                state = CollapsingToolbarLayoutState.COLLAPSED;
+                invalidateOptionsMenu();
+            } else {
+                if (state != CollapsingToolbarLayoutState.INTERNEDIATE) {
+                    binding.toolbarLayout.setTitle(article.getTitle());
+                    binding.articleTitle.setText(" ");
+                    state = CollapsingToolbarLayoutState.INTERNEDIATE;
+                    invalidateOptionsMenu();
+                }
+            }
+        });
+
+        Intent sharingIntent = new Intent().setAction(Intent.ACTION_SEND).setType("text/plain");
+        String sharingText = article.getTitle() + '\n' + article.getShare_url();
+        sharingIntent.putExtra(Intent.EXTRA_TEXT, sharingText);
+        binding.fab.setOnClickListener(v -> startActivity(Intent.createChooser(sharingIntent, article.getTitle())));
+        binding.toolbar.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                default:
+                    break;
+            }
+            return true;
+        });
     }
 }
